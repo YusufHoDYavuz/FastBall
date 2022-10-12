221f2c3391bc14f63a59c45198c4e2e2227dd4a3
Yusuf <93403926+YusufHoDYavuz@users.noreply.github.com>
2022-10-12 19:42:27 +0300

    finisih system

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -6,18 +6,19 @@ using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
-    public RectTransform grape, grapeDark;
-    public GameObject grape2, grapeDark2;
-    public Text counter;
-    float myCounter;
+    public RectTransform ballTransform, ballDarkTransform;
+    public GameObject ball, ballDark;
+    public Text counterText;
+    float counter;
+    public float ballSpeed;
 
     void Start()
     {
     }
 
-    // Update is called once per frame
     void Update()
     {
+        counterText.text = counter.ToString();
         if (Input.GetKeyDown(KeyCode.Space))
         {
             Game();
@@ -30,25 +31,41 @@ public class GameController : MonoBehaviour
 
     void Game()
     {
-        grape.transform.localPosition = new Vector3(0, 750, 0);
+
+        ballTransform.transform.localPosition = new Vector3(0, 750, 0);
         float grapeDarkRandomPos = Random.Range(-700, 400);
-        grapeDark.transform.localPosition = new Vector3(0, grapeDarkRandomPos, 0);
-        grape.transform.DOLocalMove(new Vector3(0, -750, 0), 8).OnComplete(()=> {
-            //Game();
-        });
+        ballDarkTransform.transform.localPosition = new Vector3(0, grapeDarkRandomPos, 0);
+        ballTransform.transform.DOLocalMove(new Vector3(0, -750, 0), ballSpeed);
     }
 
     void Pressed()
     {
-        grape.transform.DOKill();
-        float distance = Vector2.Distance(grape2.transform.position, grapeDark2.transform.position);
-        //float distance = grapeDark.transform.localPosition.y - grape.transform.localPosition.y;
-        if (distance <= 30)
+        ballTransform.transform.DOKill();
+        float distance = Vector2.Distance(ball.transform.position, ballDark.transform.position);
+
+        if (distance > 30)
         {
-            myCounter++;
-            counter.text = myCounter.ToString();
+            counter = 0;
             Game();
         }
+
+        if (distance <= 30)
+        {
+            counter++;
+            Game();
+        }
+
+        if (counter % 10 == 0)
+        {
+            ballSpeed--;
+            print(counter);
+        }
+
+        if (counter == 0)
+        {
+            ballSpeed = 10;
+        }
+
     }
 
 }
