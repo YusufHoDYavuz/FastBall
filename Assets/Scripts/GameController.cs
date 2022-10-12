32913286569f6e52d5eb83651b8df31b96d0a6a0
Yusuf <93403926+YusufHoDYavuz@users.noreply.github.com>
2022-10-12 22:34:29 +0300

    finisihed ui (need high score)

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -8,37 +8,27 @@ public class GameController : MonoBehaviour
 {
     public RectTransform ballTransform, ballDarkTransform;
     public GameObject ball, ballDark;
+    public GameObject startButton, pauseButton, goButton;
     public Text counterText;
     float counter;
     public float ballSpeed;
 
-    void Start()
+    public void Game()
     {
-    }
-
-    void Update()
-    {
-        counterText.text = counter.ToString();
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            Game();
-        }
-        if (Input.GetKeyDown(KeyCode.H))
-        {
-            Pressed();
-        }
-    }
-
-    void Game()
-    {
-
-        ballTransform.transform.localPosition = new Vector3(0, 750, 0);
+        ballTransform.transform.localPosition = new Vector3(0, 650, 0);
         float grapeDarkRandomPos = Random.Range(-700, 400);
         ballDarkTransform.transform.localPosition = new Vector3(0, grapeDarkRandomPos, 0);
-        ballTransform.transform.DOLocalMove(new Vector3(0, -750, 0), ballSpeed);
+        ballTransform.transform.DOLocalMove(new Vector3(0, -750, 0), ballSpeed).OnComplete(()=> {
+            ballSpeed = 10;
+            counter = 0;
+            Game();
+        });
+        startButton.SetActive(false);
+        pauseButton.SetActive(true);
+        counterText.text = counter.ToString();
     }
 
-    void Pressed()
+    public void Pressed()
     {
         ballTransform.transform.DOKill();
         float distance = Vector2.Distance(ball.transform.position, ballDark.transform.position);
@@ -65,7 +55,21 @@ public class GameController : MonoBehaviour
         {
             ballSpeed = 10;
         }
+        counterText.text = counter.ToString();
 
     }
 
+    public void PauseGame()
+    {
+        Time.timeScale = 0;
+        pauseButton.SetActive(false);
+        goButton.SetActive(true);
+    }
+
+    public void GoGame()
+    {
+        Time.timeScale = 1;
+        pauseButton.SetActive(true);
+        goButton.SetActive(false);
+    }
 }
